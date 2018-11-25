f1ab937816f2e03f09c83ca41363dcedd7289f0b
Mathieu Skulason <mathieugrettir@hotmail.com>
2018-11-25 13:04:05 +0000

    Added a method to add an emitter to the layer.

--- CAEmitterLayerApp/ViewController.swift
+++ CAEmitterLayerApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var particleImages = [UIImage]()
+    var emitter: CAEmitterLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +27,41 @@ class ViewController: UIViewController {
         
     }
     
+    func addEmitter() {
+        
+        if emitter?.superlayer != nil {
+            emitter?.removeFromSuperlayer()
+        }
+        
+        emitter = CAEmitterLayer()
+        emitter?.emitterShape = CAEmitterLayerEmitterShape.point
+        emitter?.position = CGPoint(x: self.view.frame.width * 0.5, y: self.view.frame.height * 0.5)
+        
+        var cells = [CAEmitterCell]()
+        for _ in 0..<40 {
+            let cell = CAEmitterCell()
+            cell.birthRate = 1
+            cell.lifetime = 3
+            cell.lifetimeRange = 0.5
+            cell.velocity = 500
+            cell.velocityRange = 100
+            cell.emissionRange = 2 * CGFloat(Double.pi)
+            cell.contents = getRandomImage().cgImage
+            cell.scale = 1
+            cell.scaleRange = 0.5
+            cells.append(cell)
+        }
+        
+        emitter?.emitterCells = cells
+        
+        view.layer.addSublayer( emitter! )
+        
+    }
+    
     func getRandomImage() -> UIImage {
         
-        let randomIndex = arc4random_uniform(particleImages.count + 1)
+        let upperBound = UInt32(particleImages.count + 1)
+        let randomIndex = Int(arc4random_uniform( upperBound ))
         
         return particleImages[randomIndex]
     }

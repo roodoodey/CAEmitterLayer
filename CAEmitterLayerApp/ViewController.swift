79a0beec82f20a30988e120a956f11c1816ef789
Mathieu Skulason <mathieugrettir@hotmail.com>
2018-11-25 12:54:41 +0000

    Added population to the particle images and a method to get random images.

--- CAEmitterLayerApp/ViewController.swift
+++ CAEmitterLayerApp/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var particleImages = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for index in 1..<8 {
+            if let image = UIImage(named: "StarParticle00\(index)") {
+                particleImages.append(image)
+            }
+            
+        }
+        
+        
+    }
+    
+    func getRandomImage() -> UIImage {
+        
+        let randomIndex = arc4random_uniform(particleImages.count + 1)
+        
+        return particleImages[randomIndex]
     }
 
 
